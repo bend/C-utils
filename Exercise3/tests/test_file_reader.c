95cb86a8222094dcc230d38c5dcc30a342a0df45
Benoit Daccache <ben.daccache@gmail.com>
2011-02-18 19:34:17 +0100

    tests added for buffer

--- Exercise3/tests/test_file_reader.c
+++ Exercise3/tests/test_file_reader.c
@@ -1,4 +1,5 @@
 #include "../file_reader.h"
+#include "../cracker.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +16,15 @@ void test_file_reader(){
 		assert(strcmp(get_next(f),"1 2 3 4 5 6 7 8 9 10 11 12 13")==0);
 }
 
+void test_buffer(){
+	char** bb;
+		bb= new_bounded_buffer();
+		add(bb,"hello");
+		add(bb,"hello2");
+		assert(strcmp(get(bb),"hello")===0);
+		assert(strcmp(get(bb),"hello2")==0);
+		assert(get(bb)==NULL);
+}
 
 int main(){
 
